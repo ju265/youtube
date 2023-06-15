ff04572db68a72e537729968aadc16f67f87d7e7
lm317379829 <lm317379829@163.com>
2023-06-15 10:29:18 +0800

    Add files via upload

--- main.py
+++ main.py
@@ -1,22 +1,26 @@
 import re
 import json
+import httpx
 import uvicorn
-import requests
+import asyncio
 from urllib.parse import urlparse
 from base64 import b64encode, b64decode
-from fastapi import FastAPI, Request
+from fastapi import FastAPI, Request, HTTPException
 from fastapi.responses import Response, HTMLResponse, FileResponse, RedirectResponse, StreamingResponse
 
 
+
+# 开始FastAPI及相关设置
+app = FastAPI()
+tscache = {}
 header = {
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.198 Safari/537.36"
 }
-
-
 # 获取油管播放链接
-def getplayUrl(rid, baseurl):
+async def getplayUrl(rid, baseurl):
     url = 'https://www.youtube.com/watch?v={}'.format(rid)
-    r = requests.get(url=url, headers=header, verify=False, timeout=30)
+    async with httpx.AsyncClient() as client:
+        r = await client.get(url)
     jostr_re = re.compile('var ytInitialPlayerResponse =(.*?});')
     jostr = jostr_re.findall(r.text)
     if not jostr:
@@ -31,44 +35,41 @@ def getplayUrl(rid, baseurl):
             return ''
     else:
         return ''
-    return getM3U8(url, baseurl)
+    content = await getM3U8(url, baseurl)
+    return content
 
-
-def getM3U8(url, baseurl):
-    r = requests.get(url, headers=header, stream=True, verify=False, timeout=30)
+async def getM3U8(url, baseurl):
+    async with httpx.AsyncClient() as client:
+        r = await client.get(url)
     m3u8str = ''
-    for line in r.iter_lines(8192):
-        if len(line) > 0 and not line.startswith(b'#'):
-            if line.endswith(b'.ts'):
+    tsurlList = []
+    m3u8List = r.text.splitlines()
+    for line in m3u8List:
+        if not line.startswith('#'):
+            if line.endswith('.ts'):
+                tsurlList.append(line)
                 append = '/proxymedia?url='
             else:
                 append = '/proxym3u8?url='
-            line = b64encode(line).decode()
-            m3u8str = m3u8str + baseurl + append  + line + '\n'
+            m3u8str = m3u8str + baseurl + append + b64encode(line.encode()).decode() + '\n'
         else:
-            line = line.decode()
             m3u8str = m3u8str + line + '\n'
-    r.close()
+    # 异步下载TS
+    tasks = []
+    for url in tsurlList:
+        if url not in tscache:
+            tasks.append(cachets(url))
+    await asyncio.gather(*tasks)
     return m3u8str.strip('\n')
 
 
-# 获取chunk
-def getChunk(streamable):
-    with streamable as stream:
-        stream.raise_for_status()
-        try:
-            for chunk in stream.iter_content(chunk_size=1024*1024):
-                if chunk is None:
-                    stream.close()
-                    return
-                yield chunk
-        except:
-            stream.close()
-            return
+async def cachets(url):
+    async with httpx.AsyncClient() as client:
+        r = await client.get(url)
+        content = r.content
+    tscache[url] = content
 
 
-# 开始FastAPI及相关设置
-app = FastAPI()
 # 提供 index.html 文件
 @app.get("/", response_class=HTMLResponse)
 async def index():
@@ -85,7 +86,10 @@ async def favicon():
 @app.get('/live')
 async def live(rid: str, request: Request):
     baseurl = str(request.url).split('/live')[0]
-    content = getplayUrl(rid, baseurl)
+    try:
+        content = await getplayUrl(rid, baseurl)
+    except:
+        pass
     if content == '':
         return RedirectResponse(url='http://0.0.0.0/')
     return Response(content, headers={"Content-Type": "application/vnd.apple.mpegurl", "Content-Disposition": "attachment; filename=youtube.m3u8"})
@@ -97,7 +101,10 @@ async def proxym3u8(url: str, request: Request):
     url = b64decode(url.encode()).decode()
     result = urlparse(str(request.url))
     baseurl = '{}://{}'.format(result.scheme, result.netloc)
-    content = getM3U8(url, baseurl)
+    try:
+        content = await getM3U8(url, baseurl)
+    except:
+        pass
     if content == '':
         return RedirectResponse(url='http://0.0.0.0/')
     return Response(content, headers={"Content-Type": "application/vnd.apple.mpegurl", "Content-Disposition": "attachment; filename=youtube.m3u8"})
@@ -107,27 +114,12 @@ async def proxym3u8(url: str, request: Request):
 @app.get('/proxymedia')
 async def proxymedia(url: str, request: Request):
     url = b64decode(url.encode()).decode()
-    selfheader = dict(request.headers)
-    responheader = {}
-    for key in selfheader:
-        if key.lower() in ['user-agent', 'host']:
-            continue
-        responheader[key] = selfheader[key]
-        r = requests.get(url, headers=responheader, stream=True, verify=False, timeout=30)
-    try:
-        contentType = r.headers['content-type']
-        status_code = r.status_code
-        for key in r.headers:
-            if key.lower() in ['connection', 'transfer-encoding']:
-                continue
-            if contentType.lower() in ['application/vnd.apple.mpegurl', 'application/x-mpegurl']:
-                if key.lower() in ['content-length', 'content-range', 'accept-ranges']:
-                    continue
-            responheader[key] = r.headers[key]
-        return StreamingResponse(getChunk(r), status_code=status_code, headers=responheader)
-    except:
-        r.close()
-        pass
+    if url in tscache:
+        content = tscache[url]
+        del tscache[url]
+        return Response(content=content, media_type="video/mp2t")
+    else:
+        raise HTTPException(status_code=404, detail="404 Not Found")
 
 
 if __name__ == '__main__':
